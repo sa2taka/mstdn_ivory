b8cb6512833e9c822898ca8e22defc96ed41bcc8
sa2taka <sa2taka@gmail.com>
2018-04-29 01:12:12 +0900

    Fix: Fix register method, change specifying argument

--- lib/mstdn_ivory/register.rb
+++ lib/mstdn_ivory/register.rb
@@ -3,7 +3,7 @@ require 'oauth2'
 module MstdnIvory
   module Register
     def create_app(name, scopes = 'read', redirect_uri = 'urn:ietf:wg:oauth:2.0:oob', website = nil)
-      request(:post, '/api/v1/apps', { client_name: name, redirect_uris: redirect_uri, scopes: scopes, website: website })
+      request(:post, '/api/v1/apps', form: { client_name: name, redirect_uris: redirect_uri, scopes: scopes, website: website })
     end
   end
 end
