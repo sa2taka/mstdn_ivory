5bc72de889814a9b8b3199398f2c0185196866c7
sa2taka <sa2taka@gmail.com>
2018-04-29 02:11:36 +0900

    Add: Add some documents

--- lib/mstdn_ivory/dot_accessable_hash.rb
+++ lib/mstdn_ivory/dot_accessable_hash.rb
@@ -1,4 +1,11 @@
 module MstdnIvory
+  # This class inherit hash.
+  # The value is accessed by key name method:
+  #   dah = DotAccessableHash.new
+  #   dah['strkey'] = 'strvalue'
+  #   dah[:symkey] = :symkey
+  #   dah.strkey # => "strvalue"
+  #   dah.symkey # => :symkey
   class DotAccessableHash < Hash
     def method_missing(name)
       super unless self.has_key?(name) || self.has_key?(name.to_s)
