49f8fb75cb0d636c6b4be58b357c2ce962293a7b
sa2taka <sa2taka@gmail.com>
2018-04-29 00:48:45 +0900

    Add: Add client class

---
+++ lib/mstdn_ivory/api.rb
@@ -0,0 +1,8 @@
+require 'mstdn_ivory/register'
+
+module MstdnIvory
+  module Api
+    include Register
+    include Request
+  end
+end
---
+++ lib/mstdn_ivory/client.rb
@@ -0,0 +1,18 @@
+require 'mstdn_ivory/api'
+
+module MstdnIvory
+  class Client
+    include Api
+    
+    attr_accessor :token
+
+    def initialize(base_url, token = '')
+      @base_url = base_url
+      @token = token
+    end
+
+    def headers
+      { authorization: "Bearer #{@bearer_token}" }
+    end
+  end
+end
