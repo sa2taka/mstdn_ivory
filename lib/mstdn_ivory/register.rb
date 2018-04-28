579f7b031a6a427516fdd79c40828c0a5a367dd1
sa2taka <sa2taka@gmail.com>
2018-04-29 00:48:25 +0900

    Add: Add create_app method

---
+++ lib/mstdn_ivory/register.rb
@@ -0,0 +1,9 @@
+require 'oauth2'
+
+module MstdnIvory
+  module Register
+    def create_app(name, scopes = 'read', redirect_uri = 'urn:ietf:wg:oauth:2.0:oob', website = nil)
+      request(:post, '/api/v1/apps', { client_name: name, redirect_uris: redirect_uri, scopes: scopes, website: website })
+    end
+  end
+end
